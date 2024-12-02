b839def0ccdf7cfd53782eb3db78953f550b8b54
Satyam Arora <satyamarora373@gmail.com>
2024-12-02 17:57:25 +0530

    Test

---
+++ src/components/TicketForm.js
@@ -0,0 +1,11 @@
+import React,{useState} from "react";
+
+export default function TicketForm(){
+
+    const [Ticket, setTicket]=useState([]);
+    return(
+        <div>
+            <p>Hello</p>
+        </div>
+    )
+}
