c55e68635fbd4aacde13bdc66c5d14b6af67ee7d
Satyam Arora <satyamarora373@gmail.com>
2024-12-08 15:46:45 +0530

    Added ticket form

--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -2,7 +2,15 @@ import React,{useState} from "react";
 
 export default function TicketForm(){
 
-    const [Ticket, setTicket]=useState([]);
+    const [ticket, setTicket]=useState('');
+    const [description,setDescription]=useState('');
+    const [priority,setPriority]=useState('1');
+
+    const priorityLables = {
+        1:'Low',
+        2: 'Medium',
+        3: 'High'
+    }
     return(
         <div>
             <p>Hello</p>
